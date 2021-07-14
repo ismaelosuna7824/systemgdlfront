e31defe24d02bf6d66a37e359c026c164fe2919a
ismaelosuna7824 <jioc396@gmail.com>
2021-07-14 12:27:31 -0600

    modify rutes hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,7 +41,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    relativeLinkResolution: 'legacy' 
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
